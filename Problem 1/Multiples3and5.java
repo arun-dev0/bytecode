76141baac8b7fabdef65aaf5050d3dbd87323da0
Laya <layaranjith0804@gmail.com>
2026-02-22 12:40:57 -0600

    multiples of 3 and 5 code

--- Problem 1/Multiples3and5.java
+++ Problem 1/Multiples3and5.java
@@ -4,13 +4,17 @@ public static void main (String[]args) {
   int sumOfMultiples = 0;
   
 
-for (int i=1;i<1000;i++){
-  if(i%3==0||i%5==0)
-    sumOfMultiples=sumOfMultiples+i;
-
+for (int i=3;i<1000;i +=3){// check for multiples of 3 all the way to 1000
+    sumOfMultiples=sumOfMultiples+i;//add it to the sum
 }
-System.out.println(sumOfMultiples);
+for (int i=5;i<1000;i +=5){//check for multiples of 5 all the way to 100
+  sumOfMultiples=sumOfMultiples+i;//add it to the sum
+}
+for (int i=15;i<1000;i +=15){//subtract all the reapeated multiples
+  sumOfMultiples=sumOfMultiples-i;//subtract it from the sun
+}
 
+System.out.println("The sum of all the multiples of 3 & 5 all the way to 1000 is"+sumOfMultiples);
+}
     
   }
-}
