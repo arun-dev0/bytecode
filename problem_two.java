146285f608de20a4d65a82b9e5b4a81492d4282b
AkshajDaSigma <thebossakshaj@gmail.com>
2026-02-28 13:51:08 -0600

    Create problem_two.java
    Finished the second problem

---
+++ problem_two.java
@@ -0,0 +1,27 @@
+public class problem_two {
+public static void main(String[] args) {
+    
+int limit = 4000000;
+
+int first = 1;
+int second = 2;
+
+int sum = 0;
+    
+while(second <= limit) {
+
+    if (second % 2 == 0) {
+        sum = sum + second;
+
+        }
+
+        int next = first + second;
+        first = second;
+        second = next;
+    }
+
+    System.out.println(sum);     
+
+     }    
+}
+    
