8ab95d4ffc7b1a12915421798f391c871d9c9976
Laya <layaranjith0804@gmail.com>
2026-04-11 19:01:26 -0500

    Problem 5 both questions

--- Problem 5/binary.java
+++ Problem 5/binary.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;// import the scanner
 public class binary {
-    public static void main(String[] args) {
+     
+    public static void main (String[]args) {
+        //question1();
+        question2();
+        
+    }
+     public static void question1() {
         Scanner number = new Scanner(System.in);
         System.out.println("Type in a number between 0 and 255:"); 
         int userNum = number.nextInt();// reads and stores the input number
@@ -19,5 +25,30 @@ public class binary {
         System.out.println("The Binary number is: "+binaryString);
         System.out.println("The amount of 1's in the number is: "+ count);
     }
+    public static void question2(){
+     int count=0;
+         
 
+
+        for(int i=1; i<100;i++){// loop to check all number from 1-100
+            String binaryString="";
+            int temp=i; // creating a temp variable
+            while(temp>0){ // while the temp variable is greater than 0 go into the while loop
+            int num = temp%2;// mod the temp by 2 to get the remainder (the binary number)
+            temp=temp/2;// divide it by 2 to remove all decimals
+             binaryString = num + binaryString;// add num to a binary string so you can reverse it later
+            }
+             String reversed = new StringBuilder(binaryString).reverse().toString();// reverse the binary string so you can check whether its a palindrome or not
+            if(binaryString.equals(reversed)){// check if it equals the same thing
+                count+=1;// if it is add it to the count
+                System.out.println("the binary number that is a palindrome is"+binaryString);// print all the numbers which are palindromes
+            }
+        }
+        System.out.println("Number of palindromes:"+count);// print the total count at the end
+
+
+    
+    }
 }
+
+
