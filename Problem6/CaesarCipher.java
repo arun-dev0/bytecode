b1aae9bae587e5226be454f41f5301a219dff304
gh4SiddyBOI <Sid.d.yarli+github@gmail.com>
2026-04-19 13:55:42 -0500

    Caesar Encryption
    Decryption NOT done yet but Encryption DONE for Caesar Cipher

---
+++ Problem6/CaesarCipher.java
@@ -0,0 +1,23 @@
+package Problem6;
+import java.util.Scanner;
+
+public class CaesarCipher {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    
+    System.out.println(encrypt(n, "hello"));
+    scanner.close();
+    }
+
+    public static String encrypt(int shift, String precrypt){
+        char[] arrpc = precrypt.toCharArray();
+        for(int n = 0; n < precrypt.length(); n++){
+            int x = (int) arrpc[n];
+            arrpc[n] = (char) (x + shift);
+        }
+        String encryption = new String(arrpc);
+        return encryption;
+    }
+    
+}
