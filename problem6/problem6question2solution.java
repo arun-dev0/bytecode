b00305a9e368b7d5078ace7ca835058548580d90
shatvik-a <shatvikarunkumar@gmail.com>
2026-04-27 16:41:23 -0500

    Update problem6question2solution.java

--- problem6/problem6question2solution.java
+++ problem6/problem6question2solution.java
@@ -7,15 +7,15 @@ public class problem6question2solution{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // initializes scanner to take in words
         System.out.print("Enter a word: "); // asks user for a word
-        String wordA = scanner.next(); // stores that word
+        String wordA = scanner.nextLine(); // stores that word
         System.out.print("Enter another word: "); // asks for another word
-        String wordB = scanner.next(); // stores the second word
+        String wordB = scanner.nextLine(); // stores the second word
         isAnagram(wordA, wordB); // sees if the words are anagrams
         scanner.close(); // closes the scanner
     }
     public static void isAnagram(String a, String b){ // method to determine if two words are anagrams or not
-        char[] firstWord = a.toLowerCase().toCharArray(); // turns the first word into an array and ignores case
-        char[] secondWord = b.toLowerCase().toCharArray(); // turns the second word into an array and ignores case
+        char[] firstWord = a.replaceAll(" ", "").toLowerCase().toCharArray(); // turns the first word into an array and ignores case and spaces
+        char[] secondWord = b.replaceAll(" ", "").toLowerCase().toCharArray(); // turns the second word into an array and ignores case and spaces
         Arrays.sort(firstWord); // sorts the first word
         Arrays.sort(secondWord); // sorts the second word 
         System.out.println(Arrays.equals(firstWord, secondWord) ? "The words are anagrams" : "The words are not anagrams"); // decides if the words are anagrams or not
