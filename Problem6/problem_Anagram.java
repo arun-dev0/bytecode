fc73b717163f73b77f1fc7c5d7f4a4fdb3b6fc4a
AkshajDaSigma <thebossakshaj@gmail.com>
2026-04-29 21:50:57 -0500

    Create problem_Anagram.java
    finished anagram code

---
+++ Problem6/problem_Anagram.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; // scans the words the user types
+import java.util.Arrays; // alphabetical order
+
+public class problem_Anagram {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+}       System.out.println("Please type the first word or a phrase:");
+}
